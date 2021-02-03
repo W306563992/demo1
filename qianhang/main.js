29d39cec28b19c82d580e2908595e9c2902e5d16
mona <306563992@qq.com>
2021-02-04 03:34:26 +0800

    第一版前航页面，简陋

--- qianhang/main.js
+++ qianhang/main.js
@@ -1,28 +1,50 @@
 let qianhangContent = JSON.parse(localStorage.getItem("qianhangContent"))
 if (!qianhangContent){qianhangContent = []}
-let listContent = $('#listContent')
-let addButton = $('.addButton')
+let $addButton = $('.addButton')
+let $listContent = $('#listContent')
 // 获得了qianhangContent之后需要渲染页面
-qianhangContent.forEach((item,i)=>{
-    render(item)
-})
-function render(item){
-    addButton.before(`
-    <a  href="${item.site}">
-   <li>      
-       <div class="siteName">${item.site[8]}</div>
-       <div class="site">${item.site.substring(8)}</div>
-</li>
-   </a>
-`)
-}
-addButton.on('click',()=>{
-   let site = window.prompt('请输入要添加的网址')
-   site= 'https://'+site
-   let siteitem = ({"site":site,"img":''})
-   render(siteitem)
-   qianhangContent.push(siteitem)
-localStorage.setItem('qianhangContent',JSON.stringify(qianhangContent))
-})
-// 当离开页面的时候，更新localStorage
+render()
+function render(){
+    // 渲染之前需要吧listContent 清空
+    $listContent.find('li:not(.addButton)').remove()
+    qianhangContent.forEach((item,index)=>{
+        // 添加网站列表
+        let $li = $(`
+        <li class="toSite">      
+             <div class="cancel">X</div>
+            <div class="siteName">${item.site[8].toUpperCase()}</div>
+            <div class="site">${item.site.substring(8)}</div>
+     </li>`)
+        $addButton.before($li)
+        //给li添加点击事件
+        $li.on('click',(e)=>{
+            e.stopPropagation()
+            window.open(item.site)
+        })
 
+          // 点击X就删除标签
+      $li.find('.cancel').click(function(e){
+        e.stopPropagation()
+        qianhangContent.splice(index,1)
+        console.log(qianhangContent)
+        render() 
+    })
+    })
+}
+
+$addButton.on('click',(e)=> {
+   let site = window.prompt('请输入要添加的网址')
+   if (site){
+    site= 'https://'+site
+    let siteitem = ({"site":site,"img":''})
+    
+    qianhangContent.push(siteitem)
+    localStorage.setItem('qianhangContent',JSON.stringify(qianhangContent))
+    render()
+    e.stopPropagation()
+   }
+  })
+
+window.onbeforeunload = ()=>{
+    localStorage.setItem("qianhangContent",JSON.stringify(qianhangContent))
+}
